ef4d34daed56856734a84e618b2e64ce198c30ed
Sergey Manannikov <s_manannikov@mail.ru>
2020-11-23 01:24:54 +0300

    2. Без переопределения equals и hashCode.

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,25 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Name", 2, new GregorianCalendar(1900, 0, 1));
+        User user2 = new User("Name", 2, new GregorianCalendar(1900, 0, 1));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, user1);
+        map.put(user2, user2);
+        for (User key : map.keySet()) {
+            System.out.println(key);
+        }
     }
 }
