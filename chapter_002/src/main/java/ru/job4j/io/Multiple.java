9db65fcab2d2ffdfa8e01dcbf399c14a7f609995
Sergey Manannikov <s_manannikov@mail.ru>
2020-11-28 19:37:12 +0300

    0.1. FileOutputStream.

--- chapter_002/src/main/java/ru/job4j/io/Multiple.java
+++ chapter_002/src/main/java/ru/job4j/io/Multiple.java
@@ -6,13 +6,20 @@ public class Multiple {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             out.write("1*2=2".getBytes());
+            out.write(System.lineSeparator().getBytes());
             out.write("1*3=3".getBytes());
-            out.write("1*4=2".getBytes());
-            out.write("1*5=2".getBytes());
-            out.write("1*6=2".getBytes());
-            out.write("1*7=2".getBytes());
-            out.write("1*8=2".getBytes());
-            out.write("1*9=2".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*4=4".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*5=5".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*6=6".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*7=7".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*8=8".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write("1*9=9".getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
