bdb1cb92925e7f79c45bf2e21f2bdb3e6335d13d
Sergey Manannikov <s_manannikov@mail.ru>
2020-11-29 23:04:14 +0300

    3.0. Тестирование IO.

--- chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,11 +1,10 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +12,28 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AnalyzeTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void temporaryFolder() throws IOException {
+        Analyze analyze = new Analyze();
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("200 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:02");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            assertThat(in.readLine(), is("10:58:01;10:59:02"));
+            assertThat(in.readLine(), is("11:01:02;11:02:02"));
+        }
+    }
 
     @Test
     public void whenUnavailable() throws IOException {
@@ -20,14 +41,11 @@ public class AnalyzeTest {
         String path2 = "./files/server_log_out.txt";
         Analyze analyze = new Analyze();
         analyze.unavailable(path1, path2);
-        List<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(path2))) {
-            rsl = in.lines().collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+            List<String> rsl = in.lines().collect(Collectors.toList());
+            assertThat(rsl.get(0), is("10:58:01;10:59:01"));
+            assertThat(rsl.get(1), is("11:01:02;11:02:02"));
+            assertThat(rsl.get(2), is("11:03:03;11:05:03"));
         }
-        assertThat(rsl.get(0), is("10:58:01;10:59:01"));
-        assertThat(rsl.get(1), is("11:01:02;11:02:02"));
-        assertThat(rsl.get(2), is("11:03:03;11:05:03"));
     }
 }
