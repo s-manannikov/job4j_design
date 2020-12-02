40d6b36e89e01f4df074580bcf4eef35160fecb6
Sergey Manannikov <s_manannikov@mail.ru>
2020-12-02 23:10:53 +0300

    5. Валидация параметров запуска.

--- chapter_002/src/main/java/ru/job4j/io/Dir.java
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -1,18 +1,22 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("chapter_002/files/");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
-        for (File subfile : file.listFiles()) {
+        System.out.printf("size : %s%n", file.getTotalSpace());
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
             System.out.println(subfile.getName() + ": " + subfile.length());
         }
     }
--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -7,8 +7,11 @@ import java.nio.file.Paths;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        Files.walkFileTree(start, new SearchFiles(p -> p.toString().endsWith("java")));
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        Files.walkFileTree(start, new SearchFiles(p -> p.toString().endsWith(args[1])));
         SearchFiles.list.forEach(System.out::println);
     }
 }
