796b9d1a9ff06ff652ea061fcc1111388019fe2b
Sergey Manannikov <s_manannikov@mail.ru>
2020-12-03 21:56:49 +0300

    5. Валидация параметров запуска.

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        if (args.length < 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
         Path start = Paths.get(args[0]);
