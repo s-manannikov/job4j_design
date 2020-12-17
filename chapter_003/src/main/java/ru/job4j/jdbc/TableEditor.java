04ac529d482876a75b11b8741e5d7f0c06ce1936
Sergey Manannikov <s_manannikov@mail.ru>
2020-12-18 00:00:19 +0300

    0.1. Statement.

---
+++ chapter_003/src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,101 @@
+package ru.job4j.jdbc;
+
+import org.apache.maven.settings.Settings;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class TableEditor implements AutoCloseable {
+    private Connection connection;
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws SQLException, IOException, ClassNotFoundException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws SQLException, IOException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        ClassLoader loader = Settings.class.getClassLoader();
+        try (InputStream is = loader.getResourceAsStream("app.properties")) {
+            properties.load(is);
+            String url = properties.getProperty("url");
+            String login = properties.getProperty("login");
+            String password = properties.getProperty("password");
+            connection = DriverManager.getConnection(url, login, password);
+        }
+    }
+
+    private void execute(String sql) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute(sql);
+    }
+
+    public void createTable(String tableName) throws SQLException {
+        String sql = String.format(
+                "create table if not exists %s(id serial primary key);",
+                tableName
+        );
+        execute(sql);
+    }
+
+    public void dropTable(String table) throws SQLException {
+        String sql = String.format(
+                "delete from %s;",
+                table
+        );
+        execute(sql);
+    }
+
+    public void addColumn(String tableName, String columnName, String type) throws SQLException {
+        String sql = String.format(
+                "alter table %s add if not exists %s %s;",
+                tableName,
+                columnName,
+                type
+        );
+        execute(sql);
+    }
+
+    public void dropColumn(String tableName, String columnName) throws SQLException {
+        String sql = String.format(
+                "alter table %s drop column %s;",
+                tableName,
+                columnName
+        );
+        execute(sql);
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
+        String sql = String.format(
+                "alter table %s rename column %s to %s;",
+                tableName,
+                columnName,
+                newColumnName
+        );
+        execute(sql);
+    }
+
+    public String getScheme(String tableName) throws SQLException {
+        StringBuilder scheme = new StringBuilder();
+        DatabaseMetaData metaData = connection.getMetaData();
+        try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+            scheme.append(String.format("%-15s %-15s%n", "column", "type"));
+            while (columns.next()) {
+                scheme.append(String.format("%-15s %-15s%n",
+                        columns.getString("COLUMN_NAME"),
+                        columns.getString("TYPE_NAME")));
+            }
+        }
+        return scheme.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
