b5b74b903630029acd7fa9f4064e39c9ffc7edd0
Sergey Manannikov <s_manannikov@mail.ru>
2021-01-03 20:41:08 +0300

    3. Шаблонизатор.

---
+++ chapter_005/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class GeneratorTest {
+    Generator gen = new TemplateGenerator();
+
+    @Ignore
+    @Test
+    public void whenProduce() {
+        String template = "Let the ${subject} be with ${name}!";
+        Map<String, String> map = Map.of(
+                "subject", "force",
+                "name", "you"
+        );
+        assertEquals(gen.produce(template, map), "Let the force be with you!");
+    }
+
+    @Ignore
+    @Test (expected = Exception.class)
+    public void whenNoKey() {
+        String template = "Let the ${subject} be with ${name} and ${name2}!";
+        Map<String, String> map = Map.of(
+                "subject", "force",
+                "name", "you"
+        );
+        gen.produce(template, map);
+    }
+
+    @Ignore
+    @Test (expected = Exception.class)
+    public void whenTooManyKeys() {
+        String template = "Let the ${subject} be with ${name}!";
+        Map<String, String> map = Map.of(
+                "subject", "force",
+                "name", "you",
+                "name2", "me"
+        );
+        gen.produce(template, map);
+    }
+}
