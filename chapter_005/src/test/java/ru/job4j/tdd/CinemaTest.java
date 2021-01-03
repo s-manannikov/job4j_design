9318c8bc2bfba6731a934c580e52ba733f7d3eab
Sergey Manannikov <s_manannikov@mail.ru>
2021-01-04 02:13:21 +0300

    2. Что такое TDD?

--- chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
+++ chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -9,12 +9,12 @@ import java.util.Calendar;
 import java.util.List;
 
 public class CinemaTest {
-    Account account = new AccountCinema();
-    Cinema cinema = new Cinema3D();
 
     @Ignore
     @Test
     public void whenBuyTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
@@ -24,23 +24,26 @@ public class CinemaTest {
     @Ignore
     @Test
     public void whenFindSession() {
+        Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
         assertEquals(sessions, List.of(new Session3D()));
     }
 
     @Ignore
-    @Test
+    @Test (expected = IllegalArgumentException.class)
     public void whenNoTicket() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2030, Calendar.NOVEMBER, 10, 23, 0);
-        Ticket ticket = cinema.buy(account, 1, 1, date);
-        assertNull(ticket);
+        cinema.buy(account, 1, 1, date);
     }
 
     @Ignore
     @Test
     public void whenNoSession() {
+        Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
         assertEquals(sessions.size(), 0);
